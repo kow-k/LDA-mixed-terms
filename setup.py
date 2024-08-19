780ab8ed7497056910177fcca53a57625070eff7
Kow Kuroda <kow.kuroda@gmail.com>
2024-08-19 19:43:27 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 #from distutils.core import setup # Seems like obsolete
 from setuptools import setup
 from Cython.Build import cythonize
-#setup(ext_modules = cythonize('cy_gen_ngrams.py'))
-setup(ext_modules = cythonize('cy_gen_ngrams_alt.py'))
+setup(ext_modules = cythonize('cy_gen_ngrams.py'))
+#setup(ext_modules = cythonize('cy_gen_ngrams_alt.py'))
